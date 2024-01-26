1960493cd96eb05b5230f058a39118859722ed03
Tariq Ebden <tebden16@gmail.com>
2024-01-26 13:19:04 +0000

    initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,28 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+let sum = addTwoNumbers(3, 7);
+console.log(sum);
 
 // Arrow Function With Parameters
-
+const multiplyTwoNumbers = (a, b) => {
+    return a * b;
+};
+console.log(multiplyTwoNumbers(4, 5));
 // Single Line Arrow Function With Parameters
+const subtractTwoNumbers = (a, b) => a - b;
 
+console.log(subtractTwoNumbers(19, 8));
 // Implicit Returns
-
+const saySomething = message => console.log(message);
+saySomething('Hello');
 // Returning Multiple Lines
+const multipleLines = () => (
+    `<p>
+        This is a multiline string!
+    </p>`
+)
+console.log(multipleLines());
